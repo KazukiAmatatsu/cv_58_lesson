cf312a611e15aee9f48c8fc1a372b9ca18d06f4c
KazukiAmatatsu <80532711+KazukiAmatatsu@users.noreply.github.com>
2021-04-17 11:26:32 +0900

    lesson07
    授業内容を追加

--- index.js
+++ index.js
@@ -58,3 +58,22 @@ if (yourHand % 3 === 0) {
 } else {
   console.log('あなたとはじゃんけんしません');
 }
+
+
+// 番外編
+
+// スプレッド構文
+const fruits = ['apple', 'banana', 'cherry'];
+
+const newfruits = ['grape', 'mango', ...fruits];
+
+console.log(newfruits);
+
+
+// 分割代入
+const [ hoge1, hoge2, hoge3 ] = fruits;
+// const hoge1 = fruits[0]
+// const hoge2 = fruits[1]
+// const hoge3 = fruits[2]
+
+console.log(hoge2);
